f26062288a4c521c5870e9f94b8cb05a76189b13
femke <s1154084@student.windesheim.nl>
2021-04-26 14:01:22 +0200

    Bezorgadres klasse aangemaakt, verder afstandBerekenen() methode toegevoegd: deze methode berekent de afstand tussen 2 plekken met behulp van logitude en latitude.

--- src/DatabaseConnectie.java
+++ src/DatabaseConnectie.java
@@ -1,4 +1,4 @@
-package src;
+
 import com.mysql.cj.protocol.Resultset;
 
 import java.nio.charset.StandardCharsets;
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-package src;
 
 import java.io.File;
 import java.util.ArrayList;
