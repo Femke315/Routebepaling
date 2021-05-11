6229ab984cb16dec564fe2fcf178dbb0c8e04230
Femke315 <74535442+Femke315@users.noreply.github.com>
2021-05-11 09:35:43 +0200

    SQL file met aanpassingen van de database

---
+++ Queries voor db.sql
@@ -0,0 +1,147 @@
+
+/*Toevoegen postcode kolom in people*/
+ALTER TABLE people
+ADD postcode varchar(20) Not null;
+
+use nerdygadgets;
+
+/*Toevoegen postcode tabel*/
+CREATE TABLE postcode
+(
+    PostcodeID int NOT NULL,
+    Postcode int NOT NULL,
+    PostCodePK varchar(15),
+    PostCodeNummers varchar(45),
+    PostcodeLetters varchar(45),
+    Straat varchar(50),
+    MinNummer int,
+    MaxNummer int,
+    Plaats varchar(45),
+    Gemeente Varchar(50),
+    Provincie varchar(45),
+    Latitude decimal(12,8) Not null,
+    Longitude decimal(12,8) not null,
+    CONSTRAINT PK_postcode PRIMARY KEY (PostcodeID)
+);
+
+/*Toevoegen route tabel*/
+CREATE TABLE route
+(
+    RouteID int NOT NULL auto_increment,
+    PersonID int NULL,
+    AantalPakketten int not null,
+    Reistijd decimal(3,2),
+    Afstand Varchar(10) not null,
+    Provincie varchar(45),
+    Status varchar(50) Not null,
+    Opmerkingen varchar(200) null,
+    CONSTRAINT PK_route PRIMARY KEY (RouteID),
+	CONSTRAINT check_status
+    CHECK (Status IN ("Klaar voor bezorging", "Onderweg", "Afgerond", "Anders", "Klaar voor sorteren"))
+);
+
+
+/*Kolommen RouteID & Opmerkingen toevoegen aan Orders tabel*/
+ALTER TABLE orders
+ADD RouteID int not null, ADD CONSTRAINT FOREIGN KEY(RouteID) REFERENCES Route(RouteID),
+ADD Status varchar(100) null,
+Add Opmerkingen longtext not null;
+
+/*Weet niet of de check constraint in dezelfde query kan, waarin de kolom waar het om gaat wordt gemaakt*/
+ALTER TABLE orders
+ADD CONSTRAINT CHK_status_Waarde CHECK (Status IN ("Klaar voor bezorging", "Onderweg", "Afgerond", "Anders", "Klaar voor sorteren"));
+
+
+
+/*Not null kolommen people*/
+/*emailadress*/
+ALTER TABLE people
+MODIFY EmailAddress varchar(256) NOT NULL;
+
+/*phonenumber*/
+ALTER TABLE people
+MODIFY PhoneNumber varchar(20) NOT NULL;
+
+/*IsSystemUser*/
+ALTER TABLE people
+MODIFY IsSystemUser tinyint(1) NOT NULL;
+
+/*HashedPassword*/
+ALTER TABLE people
+MODIFY HashedPassword longblob NOT NULL;
+
+/*null kolommen people*/
+/*issalesperson*/
+ALTER TABLE people
+MODIFY IsSalesperson tinyint(1) NULL;
+
+/*Preferredname*/
+ALTER TABLE people
+MODIFY PreferredName varchar(50) NULL;
+
+/*Searchname*/
+ALTER TABLE people
+MODIFY SearchName varchar(101) NULL;
+
+/*IsExternalLogonProvider*/
+ALTER TABLE people
+MODIFY IsExternalLogonProvider tinyint(1) NULL;
+
+/*check constraints*/
+ALTER TABLE route ADD CONSTRAINT check_aangewezenBezorger CHECK (PersonID); 
+
+
+
+/*bezorger, magazijn sorteerder, magazijn manager account maken*/
+CREATE user 'bezorger'@'localhost'  IDENTIFIED BY '<eigen wachtwoord>';
+CREATE user 'sorteerder'@'localhost'  IDENTIFIED BY '<eigen wachtwoord>';
+CREATE user 'magazijn_manager'@'localhost'  IDENTIFIED BY '<eigen wachtwoord>';
+
+/*grant statements bezorger*/
+GRANT SELECT ON nerdygadgets.people TO 'bezorger'@'localhost'; 
+GRANT SELECT ON nerdygadgets.personorder TO 'bezorger'@'localhost'; 
+GRANT SELECT ON nerdygadgets.orders TO 'bezorger'@'localhost'; 
+GRANT SELECT ON nerdygadgets.postcode TO 'bezorger'@'localhost'; 
+GRANT SELECT ON nerdygadgets.stockitems TO 'bezorger'@'localhost'; 
+GRANT SELECT ON nerdygadgets.stockitemholdings TO 'bezorger'@'localhost'; 
+GRANT SELECT ON nerdygadgets.orderlines TO 'bezorger'@'localhost'; 
+
+/*grant statements magazijn sorteerder*/
+GRANT SELECT ON nerdygadgets.people TO 'sorteerder '@'localhost'; 
+GRANT SELECT ON nerdygadgets.orders TO 'sorteerder '@'localhost'; 
+GRANT SELECT ON nerdygadgets.postcode TO 'sorteerder '@'localhost'; 
+GRANT SELECT ON nerdygadgets.stockitems TO 'sorteerder '@'localhost'; 
+GRANT SELECT ON nerdygadgets.stockitemholdings TO 'sorteerder '@'localhost'; 
+GRANT SELECT ON nerdygadgets.orderlines TO 'sorteerder '@'localhost';
+
+
+/*grant statements magazijn manager*/
+GRANT SELECT ON nerdygadgets.people TO 'magazijn_manager'@'localhost'; 
+GRANT SELECT, UPDATE ON nerdygadgets.orders TO 'magazijn_manager'@'localhost'; 
+GRANT SELECT ON nerdygadgets.postcode TO 'magazijn_manager'@'localhost'; 
+GRANT SELECT ON nerdygadgets.stockitems TO 'magazijn_manager'@'localhost'; 
+GRANT SELECT ON nerdygadgets.stockitemholdings TO 'magazijn_manager'@'localhost'; 
+GRANT SELECT, UPDATE ON nerdygadgets.orderlines TO 'magazijn_manager'@'localhost'; 
+GRANT SELECT, UPDATE, DELETE ON nerdygadgets.route TO 'magazijn_manager'@'localhost'; 
+GRANT SELECT, UPDATE, DELETE ON nerdygadgets.routelines TO 'magazijn_manager'@'localhost'; 
+
+FLUSH PRIVILEGES;
+
+/*alles users laten zien*/
+SELECT * FROM mysql.user;
+
+
+
+
+use nerdygadgets;
+DELIMITER // 
+CREATE TRIGGER toebedelenBezorger 
+    BEFORE UPDATE ON route 
+    FOR EACH ROW 
+       BEGIN 
+         IF new.Status ='Onderweg' AND PersonID is null 
+         THEN SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Geen bezorger toegewezen aan de route'; 
+         END IF; 
+       END; 
+ // 
+DELIMITER ; 
