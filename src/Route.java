db92644a22465fc5a466e77475c2265cc89f47d0
JobWindesheim <82949535+JobWindesheim@users.noreply.github.com>
2021-05-20 09:20:48 +0200

    Update Route.java

--- src/Route.java
+++ src/Route.java
@@ -4,7 +4,64 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Route {
+    private int routeID;
+    private int personID;
+    private int aantalPakketten;
+    private int reistijd;
+    private int afstand;
+    private String provincie;
+    private String status;
+    private String opmerkingen;
+    
     Order[] Route;
+
+
+    // Constructor met alle gegevens
+    public Route(int routeID, int personID, int aantalPakketten, int reistijd, int afstand, String provincie, String status, String opmerkingen){
+        this.routeID = routeID;
+        this.personID = personID;
+        this.aantalPakketten = aantalPakketten;
+        this.reistijd = reistijd;
+        this.afstand = afstand;
+        this.provincie = provincie;
+        this.status = status;
+        this.opmerkingen = opmerkingen;
+    }
+
+    public int getRouteID() {
+        return routeID;
+    }
+
+    public int getPersonID() {
+        return personID;
+    }
+
+    public int getAantalPakketten() {
+        return aantalPakketten;
+    }
+
+    public int getReistijd() {
+        return reistijd;
+    }
+
+    public int getAfstand() {
+        return afstand;
+    }
+
+    public String getProvincie() {
+        return provincie;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getOpmerkingen() {
+        return opmerkingen;
+    }
+    
+    
+  
 
     //constructor voor een Route. Route wordt aangemaakt doormiddel van nearest neighbour heuristic. De indexnummer is de volgordeID, de value de orderID.
     public Route(ArrayList<Order> orderlijst) {
