768f582bed748c26f9f5ea8c5797983b186e8b13
femke <s1154084@student.windesheim.nl>
2021-05-20 09:41:20 +0200

    Beide klassen samengevoegd.

--- src/Order.java
+++ src/Order.java
@@ -4,23 +4,37 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Order {
-    private String orderID;
+    private int orderID;
     private int routeID;
     private int klantID;
     private String status;
+    public String postcode;
     private double longitude;
     private double latitude;
 
+
+    public Order(String postcode, int orderID, double longitude, double latitude) {
+        this.postcode = postcode;
+        this.orderID = orderID;
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+
+    public Order(double longitude, double latitude) {
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+
     public Order(int orderID){
         DatabaseConnectie.verbindingMaken();
-        this.orderID= Integer.toString(orderID);
+        this.orderID= orderID;
 
         //create statement/query
 //        String query = "SELECT OrderID, RouteID, Status, KlantID FROM orders o INNER JOIN people p ON o.KlantID=p.PersonID WHERE p.PersonID =?";
         String query = "SELECT OrderID, Status, KlantID, RouteID FROM orders WHERE OrderID =?";
 
 
-        try ( PreparedStatement stmt = DatabaseConnectie.getConnection().prepareStatement(query))
+        try (PreparedStatement stmt = DatabaseConnectie.getConnection().prepareStatement(query))
         {
             stmt.setInt(1, orderID);//parameter toevoegen in query
 
@@ -40,40 +54,30 @@ public class Order {
         DatabaseConnectie.verbindingSluiten();
     }
 
-    public String getOrderID() {
+    public int getOrderID() {
         return orderID;
     }
 }
-=======
-package src;
-import java.util.ArrayList;
-
-public class Order {
-
-        public double x;
-        public double y;
-        public String postcode;
-        public int orderID;
-        public int routeID;
-
-        public Order(String postcode, int orderID, double x, double y) {
-            this.postcode = postcode;
-            this.orderID = orderID;
-            this.x = x;
-            this.y = y;
-        }
-
-        public Order(double x, double y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-
-
-
-
-
-
-
-
->>>>>>> origin/Distributiemedewerker_&_Route
+//=======
+//package src;
+//import java.util.ArrayList;
+//
+//public class Order {
+//
+//        public double x;
+//        public double y;
+//
+//        public int orderID;
+//        public int routeID;
+//
+//
+//    }
+//
+//
+//
+//
+//
+//
+//
+//
+//>>>>>>> origin/Distributiemedewerker_&_Route
