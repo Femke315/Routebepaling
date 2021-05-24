62af943854515d8b080c05b9c5ef4ab6c6c7e1f9
Femke315 <74535442+Femke315@users.noreply.github.com>
2021-05-24 14:07:27 +0200

    afstand-->int

--- Queries voor db.sql
+++ Queries voor db.sql
@@ -37,7 +37,7 @@ CREATE TABLE route
     PersonID int NULL,
     AantalPakketten int not null,
     Reistijd decimal(4,2),
-    Afstand decimal(5,2) not null,
+    Afstand int(11) not null,
     Provincie varchar(45) not null,
     Status varchar(50) Not null,
     Opmerkingen varchar(200) null,
