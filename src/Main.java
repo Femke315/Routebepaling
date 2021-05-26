a4e1576119c79dd2508f800b169577c6e2f7ad2b
femke <s1154084@student.windesheim.nl>
2021-05-26 18:42:01 +0200

    Methode gemaakt die een route verwijderd uit de applicatie.

--- src/Main.java
+++ src/Main.java
@@ -16,12 +16,13 @@ public class Main {
 //
 //        Boolean verbindingSluiten = DatabaseConnectie.verbindingSluiten();
 
-        HoofdschermGUI Scherm = new HoofdschermGUI();
+//        HoofdschermGUI Scherm = new HoofdschermGUI();
 
-//        SQLqueries queries= new SQLqueries();//connectie wordt hierin gemaakt
+        SQLqueries queries= new SQLqueries();//connectie wordt hierin gemaakt
 //        queries.getProducts(37);//test getProduct()
 //        queries.getRoutes("Klaar voor sorteren");
 //        queries.getpeople(3146);//Connectie wordt hierin gesloten
+        SQLqueries.routeVerwijderen(1);
     }
 
 
